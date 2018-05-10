4ad1543a8303ef70cb6bace8299efb529a0f5315
ESSARRAJ <essarraj.fouad@gmail.com>
2018-05-10 11:47:01 +0200

    Create Multitier architecture

---
+++ TrainingIS.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrainingIS.WebApp.Startup))]
+namespace TrainingIS.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ TrainningIS.DAL/TrainingISModel.cs
@@ -0,0 +1,30 @@
+﻿namespace TrainningIS.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class TrainingISModel : DbContext
+    {
+     
+        public TrainingISModel()
+            : base(@"data source=(LocalDb)\MSSQLLocalDB;initial catalog=TrainingIS;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework")
+        {
+        }
+
+
+        #region Singleton
+        private static TrainingISModel _ContextInstance;
+        public static TrainingISModel CreateContext()
+        {
+            if (_ContextInstance == null)
+            {
+                _ContextInstance = new TrainingISModel();
+                return _ContextInstance;
+            }
+            else return _ContextInstance;
+        }
+        #endregion
+
+    }
+}
