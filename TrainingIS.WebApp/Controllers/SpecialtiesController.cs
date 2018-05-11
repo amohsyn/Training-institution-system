85502cb6152d45891dcae8495764c375e9a6a124
ES-SARRAJ <essarraj.fouad@gmail.com>
2018-05-11 12:59:04 +0200

    Generate Speciality Scaffolding

---
+++ TrainingIS.WebApp/Controllers/SpecialtiesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrainingIS.DAL;
+using TrainingIS.Entities;
+
+namespace TrainingIS.WebApp.Controllers
+{
+    public class SpecialtiesController : Controller
+    {
+        private TrainingISModel db = new TrainingISModel();
+
+        // GET: Specialties
+        public ActionResult Index()
+        {
+            return View(db.Specialtys.ToList());
+        }
+
+        // GET: Specialties/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Specialty specialty = db.Specialtys.Find(id);
+            if (specialty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(specialty);
+        }
+
+        // GET: Specialties/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Specialties/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Code,Reference,Ordre,DateCreation,DateModification")] Specialty specialty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Specialtys.Add(specialty);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(specialty);
+        }
+
+        // GET: Specialties/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Specialty specialty = db.Specialtys.Find(id);
+            if (specialty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(specialty);
+        }
+
+        // POST: Specialties/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Code,Reference,Ordre,DateCreation,DateModification")] Specialty specialty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(specialty).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(specialty);
+        }
+
+        // GET: Specialties/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Specialty specialty = db.Specialtys.Find(id);
+            if (specialty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(specialty);
+        }
+
+        // POST: Specialties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Specialty specialty = db.Specialtys.Find(id);
+            db.Specialtys.Remove(specialty);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
