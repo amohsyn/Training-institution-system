37d1171dae491ad85e67041c0b7a3f2728ef6308
ESSARRAJ <essarraj.fouad@gmail.com>
2018-08-09 18:18:48 +0100

    Change Test File Name Visibility

---
+++ TrainingIS.WebAppTests/Controllers/Generated/Export_RoleAppsControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingIS.WebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TrainingIS.Entities;
+using AutoFixture;
+using TrainingIS.BLL;
+using TrainingIS.DAL;
+using TrainingIS.WebApp.Tests.ViewModels;
+using System.ComponentModel.DataAnnotations;
+using TrainingIS.WebApp.Helpers.AlertMessages;
+using GApp.WebApp.Tests;
+using GApp.WebApp.Manager.Views;
+using TrainingIS.WebApp.Tests.TestUtilities;
+using TrainingIS.Entities.ModelsViews;
+using TrainingIS.BLL.ModelsViews;
+
+namespace TrainingIS.WebApp.Controllers.Tests
+{
+    [TestClass()]
+    public class Export_RoleAppsControllerTests : ManagerControllerTests
+    {
+		RoleAppsControllerTests_Service TestService = new RoleAppsControllerTests_Service();
+
+    }
+}
+
