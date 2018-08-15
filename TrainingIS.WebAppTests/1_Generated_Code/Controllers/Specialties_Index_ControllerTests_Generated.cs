eb5f3684e1f2d6d9f8289a90d3391f4ad0801ac9
ESSARRAJ <essarraj.fouad@gmail.com>
2018-08-15 13:40:14 +0100

    Clean Index_Controllers_Tests

---
+++ TrainingIS.WebAppTests/1_Generated_Code/Controllers/Specialties_Index_ControllerTests_Generated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingIS.WebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TrainingIS.Entities;
+using AutoFixture;
+using TrainingIS.BLL;
+using TrainingIS.DAL;
+using TrainingIS.WebApp.Tests.ViewModels;
+using System.ComponentModel.DataAnnotations;
+using GApp.WebApp.Tests;
+using GApp.WebApp.Manager.Views;
+using TrainingIS.WebApp.Tests.TestUtilities;
+using TrainingIS.Entities.ModelsViews;
+using TrainingIS.BLL.ModelsViews;
+
+namespace TrainingIS.WebApp.Controllers.Tests
+{
+    [TestClass()]
+    public class Specialties_Index_ControllerTests : ManagerControllerTests
+    {
+
+
+
+    }
+}
+
